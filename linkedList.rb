439c16c0f815ea00d69641e011c034de1b6026fb
Ali <alimkali.alizhan@gmail.com>
2020-05-22 00:50:32 +0600

    insert.at and remove.at features added

--- linkedList.rb
+++ linkedList.rb
@@ -93,6 +93,59 @@ class LinkedList
         print "nil\n"
     end
 
+    def insert_at(value, index)
+        if index > @size
+            return false
+        end
+
+        node = Node.new(value)
+
+        if index == @size
+            @tail = node
+        end
+
+        if index == 1
+            @size += 1
+            node = Node.new(value,@head)
+            @head = node
+            return 
+        end
+
+        x = index - 2 
+        current = @head
+
+        x.times do 
+            current = current.next_node
+        end
+        nextnode = current.next_node
+        current.next_node = node
+        node.next_node = nextnode
+        @size += 1
+    end
+
+    def remove_at(index)
+        current = @head
+        x = index - 2
+        if index > @size
+            false
+        end
+        if index == 1
+            @head = current.next_node
+            return
+        end
+        
+        x.times do 
+            current = current.next_node    
+        end
+        nextnext = current.next_node.next_node
+        current.next_node = nextnext
+
+        if index == @size
+            @tail = current
+        end
+        @size -= 1
+    end
+
 end
 
 class Node
@@ -115,6 +168,6 @@ puts a.size
 puts a.head
 puts a.at(3)
 puts a.contains?("Ers")
-
-
+a.remove_at(2)
+a.insert_at("Erss",2)
 a.to_s
